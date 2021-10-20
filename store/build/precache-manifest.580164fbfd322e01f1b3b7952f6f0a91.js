ca11920e89bf3735511b830616d867b31cb6cea2
Bryan Lara <larabbryan@gmail.com>
2021-10-20 01:53:26 -0500

    Adding store progress

---
+++ store/build/precache-manifest.580164fbfd322e01f1b3b7952f6f0a91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914893895984fc604fe774ab07c388fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cfc4709f47308bb813b",
+    "url": "/static/css/main.24fe24d7.chunk.css"
+  },
+  {
+    "revision": "42e7697be07d0745a601",
+    "url": "/static/js/2.012da98c.chunk.js"
+  },
+  {
+    "revision": "4cfc4709f47308bb813b",
+    "url": "/static/js/main.b1e6ba89.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
